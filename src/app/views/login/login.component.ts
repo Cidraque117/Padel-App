aef8b625ff80bfd5e20cc5fd86a7d16860c7848d
JuanFSanz <juanfransc.work@hotmail.com>
2025-05-30 10:06:48 +0200

    pequeños cambios

--- src/app/views/login/login.component.ts
+++ src/app/views/login/login.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { Login } from '../../interfaces/login.interface';
 import { AuthService } from '../../services/auth.service';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
@@ -13,26 +14,33 @@ export class LoginComponent {
 
   constructor(public service: AuthService) { }
 
-  public loginData: Login = {
-    email: '',
-    password: '',
-    error: ''
-  };
+  public loginForm: FormGroup = new FormGroup({
+    email: new FormControl(''),
+    password: new FormControl('')
+  });
 
-  public login(): void {
-    this.loginData.error = ''; // LIMPIAR ERRORES, PREVIOS
+  public errorMessage: string = '';
 
-    this.service.login(this.loginData).subscribe({
+  public onSubmit(): void {
+    this.errorMessage = '';
+
+    if (this.loginForm.invalid) {
+      this.errorMessage = 'Completa todos los campos correctamente.';
+      return;
+    }
+
+    console.log("FORM: ", this.loginForm.value);
+
+
+    this.service.login(this.loginForm.value).subscribe({
       next: (response) => {
-        console.log('Respuesta del servidor: ', response);
-        // GUARDAR TOKEN/ REDIRIGIR
+        console.log('Respuesta del servidor:', response);
+        // TOKEN Y REDIRIGIR
       },
       error: (err) => {
-        console.error('Error del servidor: ', err);
-        this.loginData.error = 'Credenciales incorrectas o error del servidor';
+        console.error('Error en login:', err);
+        this.errorMessage = 'Credenciales incorrectas o error del servidor';
       }
     });
   }
-
-
 }
